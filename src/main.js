48a093f59d3255feff55f722a2c906ef37851f2e
Alex Roy <LargeOrange@github.com>
2026-09-20 21:08:44 -0400

    forms stable network

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import * as THREE from 'three';
 
 import splatMatFragSrc from './splatmat.frag.glsl?raw'
-import { atomicAdd, atomicLoad, atomicStore, float, Fn, instancedArray, instanceIndex, radians, texture, textureStore, uint, uvec2, vec4, vec2, cos, sin, atan, If } from 'three/tsl';
+import { atomicAdd, atomicLoad, atomicStore, float, Fn, instancedArray, instanceIndex, radians, texture, textureStore, uint, uvec2, vec4, vec2, cos, sin, atan, If, uniform, hash, round } from 'three/tsl';
 import { MeshBasicNodeMaterial, StorageTexture, WebGPURenderer } from 'three/webgpu';
 
 // -------------- TUNABLES -----------------------------
 const WIDTH = 1280;
 const HEIGHT = 720;
-const STEP_SIZE = 5;
-const TURN_RAD = 10;
-const DECAY_RATE = 0.05;
+const STEP_SIZE = 1;
+const TURN_RAD = 45;
+const DECAY_RATE = 0.1;
 const SENSOR_DIST = 10;
-const SENSOR_ANGLE = 25;
+const SENSOR_ANGLE = 50;
 const W = 0.2;
-const DROP = 20;
+const RANDOM_TURN = 30;
 
-const count = 360000;
+const count = 300000;
 
 const renderer = new WebGPURenderer();
 await renderer.init();
@@ -25,6 +25,7 @@ document.body.appendChild(renderer.domElement);
 
 // -------------- SIMULATION ---------------------------
 const degToRad = degrees => degrees * (Math.PI / 180);
+
 
 const startBuffer = new Float32Array(4 * count);
 for (let i = 0; i < count; i++) {
@@ -38,15 +39,12 @@ for (let i = 0; i < count; i++) {
 const agentBuffer = instancedArray(startBuffer, 'vec4');
 const trailMap = instancedArray(WIDTH * HEIGHT, 'uint').toAtomic();
 const trailBuffer = instancedArray(WIDTH * HEIGHT, 'float');
+const frame = uniform(0, 'uint');
 
 const trailMapTexture = new StorageTexture(WIDTH, HEIGHT);
 trailMapTexture.magFilter = THREE.NearestFilter;
 trailMapTexture.minFilter = THREE.NearestFilter;
 
-const createAgentNode = Fn(() => {
-  const currentAgent = agentBuffer.element(instanceIndex);
-  currentAgent.assign(vec4(WIDTH/2, HEIGHT/2, cos(radians(instanceIndex.toFloat().mul(0.001))), sin(radians(instanceIndex.toFloat().mul(0.001)))));
-})().compute(count);
 
 
 const agentNode = Fn(() => {
@@ -55,20 +53,20 @@ const agentNode = Fn(() => {
   const posX = currentAgent.x;
   const posY = currentAgent.y;
 
-  const dirX = currentAgent.z;
-  const dirY = currentAgent.w;
-
+  const random_dir = hash(hash(frame).mul(100000).add(instanceIndex)).mul(2).sub(1).mul(degToRad(RANDOM_TURN)).toVar();
+  const coin_flip = round(hash(random_dir.mul(100000).add(12380912))).mul(2).sub(1).toVar();
 
 
   let final_angle = float(0).toVar();
-  const dir_angle = atan(dirY, dirX).toVar();
-  const left_sensor_angle = dir_angle.sub(degToRad(SENSOR_ANGLE));
-  const right_sensor_angle = dir_angle.add(degToRad(SENSOR_ANGLE));
+  const front_dir_angle = atan(currentAgent.w, currentAgent.z).add(random_dir).toVar();
+  const left_sensor_angle = front_dir_angle.sub(degToRad(SENSOR_ANGLE));
+  const right_sensor_angle = front_dir_angle.add(degToRad(SENSOR_ANGLE));
 
+  const front_sensor_dir = vec2(cos(front_dir_angle), sin(front_dir_angle));
   const left_sensor_dir = vec2(cos(left_sensor_angle), sin(left_sensor_angle));
   const right_sensor_dir = vec2(cos(right_sensor_angle), sin(right_sensor_angle));
 
-  const front_sensor = posY.add(dirY.mul(SENSOR_DIST)).add(HEIGHT).mod(HEIGHT).floor().mul(WIDTH).add(posX.add(dirX.mul(SENSOR_DIST)).add(WIDTH).mod(WIDTH).floor()); // (y + dirY * SENSOR_DIST) * WIDTH + (posX + dirX * SENSOR_DIST)
+  const front_sensor = posY.add(front_sensor_dir.y.mul(SENSOR_DIST)).add(HEIGHT).mod(HEIGHT).floor().mul(WIDTH).add(posX.add(front_sensor_dir.x.mul(SENSOR_DIST)).add(WIDTH).mod(WIDTH).floor()); // (y + dirY * SENSOR_DIST) * WIDTH + (posX + dirX * SENSOR_DIST)
   const left_sensor = posY.add(left_sensor_dir.y.mul(SENSOR_DIST)).add(HEIGHT).mod(HEIGHT).floor().mul(WIDTH).add(posX.add(left_sensor_dir.x.mul(SENSOR_DIST)).add(WIDTH).mod(WIDTH).floor()); // (y + sin(left_sensor_angle) * SENSOR_DIST) * WIDTH + (posX + cos(left_sensor_angle) * SENSOR_DIST)
   const right_sensor = posY.add(right_sensor_dir.y.mul(SENSOR_DIST)).add(HEIGHT).mod(HEIGHT).floor().mul(WIDTH).add(posX.add(right_sensor_dir.x.mul(SENSOR_DIST)).add(WIDTH).mod(WIDTH).floor()); // (y + sin(right_sensor_angle) * SENSOR_DIST) * WIDTH + (posX + cos(right_sensor_angle) * SENSOR_DIST)
 
@@ -78,10 +76,13 @@ const agentNode = Fn(() => {
   const right_sensor_value = trailBuffer.element(right_sensor).toVar();
 
   If(left_sensor_value.greaterThan(front_sensor_value).and(left_sensor_value.greaterThan(right_sensor_value)),
-    () => { final_angle.assign(dir_angle.sub(degToRad(TURN_RAD))) }).
+    () => { final_angle.assign(front_dir_angle.sub(degToRad(TURN_RAD))) }).
     ElseIf(right_sensor_value.greaterThan(front_sensor_value).and(right_sensor_value.greaterThan(left_sensor_value)),
-      () => { final_angle.assign(dir_angle.add(degToRad(TURN_RAD))) }).
-    Else(() => { final_angle.assign(dir_angle) });
+      () => { final_angle.assign(front_dir_angle.add(degToRad(TURN_RAD))) }).
+    ElseIf(front_sensor_value.greaterThan(left_sensor_value).and(front_sensor_value.greaterThan(right_sensor_value)),
+      () => { final_angle.assign(front_dir_angle) }).
+    Else(
+      () => { final_angle.assign(front_dir_angle.add(float(degToRad(TURN_RAD)).mul(coin_flip))) } );
 
   const newPos = vec4(
     posX.add(cos(final_angle).mul(STEP_SIZE)).add(WIDTH).mod(WIDTH),
@@ -110,10 +111,11 @@ const trailMapTextureNode = Fn(() => {
   }
 
   const prev = trailBuffer.element(instanceIndex);
-  const v = sum.div(9).mul(float(1 - DECAY_RATE)).add(float(atomicLoad(trailMap.element(instanceIndex)))).toVar();
-  prev.assign(float(1-W).mul(prev).add(v.mul(W)));
+  const mixed = prev.mul(1 - W).add(sum.div(9).mul(W));
+  const v = mixed.mul(1 - DECAY_RATE).add(float(atomicLoad(trailMap.element(instanceIndex)))).toVar();
+  prev.assign(v);
 
-  const out = v.div(DROP).toVar();
+  const out = v.div(v.add(1)).toVar();
   textureStore(trailMapTexture, uvec2(x.toUint(), y.toUint()), vec4(out, out, out, 1)).toWriteOnly();
 })().compute(WIDTH * HEIGHT);
 
@@ -139,7 +141,7 @@ mesh.frustumCulled = false;
 
 renderer.setAnimationLoop((now) => {
   //renderer.compute(agentNode, [count, 1, 1]);
-  //
+  frame.value = (frame.value + 1) % 5000;
   renderer.compute(agentNode);
   renderer.compute(trailMapTextureNode);
   renderer.compute(clearTrailMapNode);
